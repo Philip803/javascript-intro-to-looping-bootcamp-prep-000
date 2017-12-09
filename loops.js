41b2a43af47dfe14c71fa52fbf5a63c722daf492
philip ka sing leung <philip.leung90@gmail.com>
2017-12-09 21:36:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(){
   var array = [];
   for(let i = 0 ; i < 25 ; i++){
-    array.push =  'I am ${i === 0 ? 1 : i + 1} strange loop.'
+    array.push =  'I am ${i === 0 ? "1" : "i + 1"} strange loop.'
   }
   return array;
 }
